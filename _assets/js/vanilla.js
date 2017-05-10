8963afe8c30a2b588c45fbfa0e1c2d205a9691f1
David Carter <dajocarter@gmail.com>
2017-05-10 00:09:22 -0400

    Add waypoint for header class switch

--- _assets/js/vanilla.js
+++ _assets/js/vanilla.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM Content Loaded');
+  var waypoint = new Waypoint({
+    element: document.getElementsByClassName('container')[0],
+    handler: function( direction ) {
+      if ( direction === 'down' ) {
+        document.getElementsByClassName('navMenu')[0].classList.add('white');
+      } else {
+        document.getElementsByClassName('navMenu')[0].classList.remove('white');
+      }
+    },
+    offset: 42
+  });
 });
