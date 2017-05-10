4777eb0ffcdbd6aa585b4a52d149c232c12c355c
David Carter <dajocarter@gmail.com>
2017-05-10 00:06:14 -0400

    Install waypoints

--- gulpfile.js
+++ gulpfile.js
@@ -136,7 +136,10 @@ gulp.task( 'build:scripts', () =>
     'node_modules/jquery/dist/jquery.js',
     'node_modules/tether/dist/js/tether.js',
     'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/instafeed.js/instafeed.js',
+    'node_modules/waypoints/lib/jquery.waypoints.js',
+    'node_modules/waypoints/lib/shortcuts/infinite.js',
+    'node_modules/waypoints/lib/shortcuts/inview.js',
+    'node_modules/waypoints/lib/shortcuts/sticky.js',
     // Add all npm packages first
     '_assets/js/**/*.js'
   ] )
